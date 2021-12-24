1cf041cb5f0ce1bfe8e7e2f9f751f550cde09437
AlexGlazunov <dagget19dk@gmail.com>
2021-12-24 19:52:53 +0300

    still ok

--- StackChat/Controller/Login/RegViewController.swift
+++ StackChat/Controller/Login/RegViewController.swift
@@ -8,16 +8,34 @@
 import UIKit
 
 class RegViewController: UIViewController {
+    
     var delegate: LoginViewControllerDelegate!
+    
+    @IBOutlet var mainView: UIView!
+    var tapGest: UIGestureRecognizer?
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var rePasswordField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tapGest = UIGestureRecognizer(target: self, action: #selector(endEditing))
+        mainView.addGestureRecognizer(tapGest!)
         
     }
     
     @IBAction func closeVC(_ sender: Any) {
         delegate.closeVC()
     }
-    //delegate//
-
+    
+    @objc func endEditing(){
+        self.view.endEditing(true)
+        
+    }
+    @IBAction func regBtnClick(_ sender: Any) {
+    }
+    
 }
